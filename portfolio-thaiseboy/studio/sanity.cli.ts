cf60ccef3d1d4f131f3290bc29e2aad8be9cb2d6
Thaiseboy <get_sarun@hotmail.com>
2024-10-16 00:24:23 +0200

    Sanity Studio toegevoegd

---
+++ portfolio-thaiseboy/studio/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'w696z6zz',
+    dataset: 'portfolio-thaiseboy'
+  }
+})
---
+++ portfolio-thaiseboy/studio/schemaTypes/index.ts
@@ -0,0 +1,5 @@
+import skill from './skill'
+import project from './project' // Voeg je project schema hier toe
+import contact from './contact'
+
+export const schemaTypes = [skill, project, contact] // Voeg project hier toe
