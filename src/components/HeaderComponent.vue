076afde01be6512c8bc6d7dac012d6952da4c11e
Thaiseboy <get_sarun@hotmail.com>
2024-10-19 01:59:28 +0200

    Change Logo

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -2,7 +2,7 @@
   <header class="text-gold py-5">
     <div class="container d-flex flex-column align-items-center">
       <div class="text-center">
-        <img src="@/assets/foto/logo.png" alt="Logo" class="logo-img" />
+        <img src="@/assets/foto/logo2.png" alt="Logo" class="logo-img" />
       </div>
     </div>
   </header>
