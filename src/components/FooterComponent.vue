8ab1c18df45b6359844b0e675143fa3549e521a8
Thaiseboy <get_sarun@hotmail.com>
2024-11-19 17:19:33 +0100

    Footer text aangepast

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -2,7 +2,7 @@
   <footer class="bg-black text-gold text-center py-4">
     <div class="container">
       <p class="mb-0 text-gold">&copy; 2024 Portfolio. Master Supakon Karanyawad</p>
-      <p class="mb-0">This portfolio uses <span class="text-red"> Vue.js &#x2016; Sanity</span></p>
+      <p class="mb-0">This portfolio uses <span class="text-gold">Framework </span><span class="text-red"> Vue.js <span class="text-gold">&amp;</span> Sanity</span><span class="text-gold"> Headless CMS</span></p>
     </div>
   </footer>
 </template>
