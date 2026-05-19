201421826f4fe9f68be36d2f087088774894fd97
Get Karanyawad <thaiseboy@gmail.com>
2026-05-19 21:49:24 +0200

    Change year copy right

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -2,10 +2,13 @@
   <footer class="bg-black text-gold text-center py-4">
     <div class="container">
       <p class="mb-0 text-gold">
-        &copy; 2024 Portfolio. Master Supakon Karanyawad
+        &copy; 2026 Portfolio. Master Supakon Karanyawad
       </p>
       <p class="mb-0">
-        This portfolio uses <span class="text-gold">Framework </span><span class="text-red"> Vue.js <span class="text-gold">&amp;</span> Sanity</span><span class="text-gold"> Headless CMS</span>
+        This portfolio uses <span class="text-gold">Framework </span
+        ><span class="text-red">
+          Vue.js <span class="text-gold">&amp;</span> Sanity</span
+        ><span class="text-gold"> Headless CMS</span>
       </p>
     </div>
   </footer>
@@ -13,6 +16,6 @@
 
 <script setup>
 defineOptions({
-  name: 'AppFooter'
+  name: "AppFooter",
 });
 </script>
